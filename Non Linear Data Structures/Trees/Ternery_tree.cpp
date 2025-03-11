6bb88c794bbe7bbb24248633f0e1c6eac835741b
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-11 23:52:38 +0530

    ternery tree

---
+++ Non Linear Data Structures/Trees/Ternery_tree.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public: 
+        int data;
+        Node* left;
+        Node* mid;
+        Node* right;
+    Node(int data) {
+        this->data = data;
+        this->left = nullptr;
+        this->mid = nullptr;
+        this->right = nullptr;
+    }
+};
+int main() {
+
+    Node* root = new Node(10);
+    root->left = new Node(11);
+    root->mid = new Node(66);
+    root->right = new Node(21);
+
+    root->left->left = new Node(90);
+    root->left->mid = new Node(91);
+    root->left->right = new Node(97);
+
+    root->mid->left = new Node(80);
+    root->mid->mid = new Node(81);
+    root->mid->right = new Node(87);
+    
+    root->right->left = new Node(70);
+    root->right->mid = new Node(71);
+    root->right->right = new Node(77);
+
+    cout<<root->data<<endl;
+
+    return 0;
+}
