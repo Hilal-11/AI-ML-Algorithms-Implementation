b92b51561d70b6612c76c733afb6031a819323a5
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-11 23:19:09 +0530

    Binary tree

--- Non Linear Data Structures/Trees/Trees.cpp
+++ Non Linear Data Structures/Trees/Trees.cpp
@@ -8,8 +8,20 @@ class Node {
         Node* second_child;
         Node* third_child;
         Node* forth_child;
+
+    Node(int data) {
+        this->data = data;
+        this->first_child = NULL;
+        this->second_child = NULL;
+        this->third_child = NULL;
+        this->forth_child = NULL;
+    }
 };
 int main () {
-    
+
+    Node* root = NULL;
+    root->data = 10;
+    cout<<root->data;
+
     return 0;
 }
