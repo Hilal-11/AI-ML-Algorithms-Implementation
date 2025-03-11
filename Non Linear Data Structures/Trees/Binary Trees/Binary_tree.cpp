2246c6e5375bef2c65fbf079ca887fe57b166cb6
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-11 23:58:53 +0530

    bt

--- Non Linear Data Structures/Trees/Binary Trees/Binary_tree.cpp
+++ Non Linear Data Structures/Trees/Binary Trees/Binary_tree.cpp
@@ -18,10 +18,22 @@ int main () {
     root->left = new Node(12);
     root->right = new Node(21);
 
+    root->left->left = new Node(33);
+    root->left->right = new Node(33);
+
+    root->right->left = new Node(53);
+    root->right->right = new Node(63);
+
     cout<<root->data<<endl;
     cout<<root->left->data<<"\t";
     cout<<root->right->data<<endl;
 
+    cout<<root->left->left->data<<"\t";
+    cout<<root->right->right->data<<endl;
+
+
+    cout<<root->left->left->left->data<<"\t";
+    cout<<root->right->right->right->data<<endl;
 
     
     return 0;
