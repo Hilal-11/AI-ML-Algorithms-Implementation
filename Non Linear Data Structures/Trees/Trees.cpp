9fbd7b7aea86ef5c385d92c71251d03c255b4c54
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-11 22:37:38 +0530

    trees implementation

---
+++ Non Linear Data Structures/Trees/Trees.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public :
+        int data;
+        Node* first_child;
+        Node* second_child;
+        Node* third_child;
+        Node* forth_child;
+};
+int main () {
+    
+    return 0;
+}
