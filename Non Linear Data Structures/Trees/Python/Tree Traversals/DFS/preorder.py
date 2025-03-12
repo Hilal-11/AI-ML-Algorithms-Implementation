2ceb7edb92c66c04d71ad59031ab06ae10afb482
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-12 23:26:24 +0530

    pre order .py

--- Non Linear Data Structures/Trees/Python/Tree Traversals/DFS/preorder.py
+++ Non Linear Data Structures/Trees/Python/Tree Traversals/DFS/preorder.py
@@ -6,15 +6,20 @@ class Node :
         self.left = None
         self.right = None
         
-def preOrderTraversal() :
-    pass    
+def preOrderTraversal(root) :
+    if root == None:
+        return
+    print(root.data)
+    preOrderTraversal(root.left)
+    preOrderTraversal(root.right)
 
 root = Node(10)
 root.left = Node(11)
 root.right = Node(22)
 
-print(root.data)
-print(root.left.data)
-print(root.right.data)
+# print(root.data)
+# print(root.left.data)
+# print(root.right.data)
+preOrderTraversal(root)
 
 
