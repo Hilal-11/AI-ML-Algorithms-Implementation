b9ea586a1b0c28aec7382503e8f7dbe3a768c9bb
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-12 23:24:10 +0530

    traversals in .py

---
+++ Non Linear Data Structures/Trees/Python/Tree Traversals/DFS/inorder.py
@@ -0,0 +1,16 @@
+class Node :
+    def __init__(self , data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+def preOrderTraversal() :
+    pass    
+
+root = Node(10)
+root.left = Node(11)
+root.right = Node(22)
+
+print(root.data)
+print(root.left.data)
+print(root.right.data)
