8705cf4151bcfca73e87835e748d718ebead5171
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-12 23:03:01 +0530

    post order traversal iteratively

--- Non Linear Data Structures/Trees/C++/Tree Traversals/DFS/postorder.cpp
+++ Non Linear Data Structures/Trees/C++/Tree Traversals/DFS/postorder.cpp
@@ -15,12 +15,34 @@ class Node {
     }
 };
 
+// void postOrderTraversal(Node* root) {
+//     if(root == NULL) return;
+
+//     postOrderTraversal(root->left);
+//     postOrderTraversal(root->right);
+//     cout<<root->data<<"\t";
+// }
+
 void postOrderTraversal(Node* root) {
     if(root == NULL) return;
 
-    postOrderTraversal(root->left);
-    postOrderTraversal(root->right);
-    cout<<root->data<<"\t";
+    stack<Node*> st1;
+    stack<Node*> st2;
+
+    st1.push(root);
+
+    while(!st1.empty()) {
+        Node* node = st1.top();
+        st1.pop();
+        st2.push(node);
+
+        if(node->left != NULL) st1.push(node->left);
+        if(node->right != NULL) st1.push(node->right);
+    }
+    while(!st2.empty()) {
+        cout<<st2.top()->data<<"\t";
+        st2.pop();
+    }
 }
 
 int main () {
