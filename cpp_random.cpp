2baec3e297f56b4c70b94934380e24224bb5cd29
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-15 20:17:39 +0530

    cpp random multiple generators

--- cpp_random.cpp
+++ cpp_random.cpp
@@ -29,6 +29,24 @@ int main() {
     }
 
 
+    // Generating Random Numbers in a Specific Range
+        //  random number=min+(rand()%(max−min+1))
+
+    // example
+
+    int random_number = 1+(rand() % (100-1+1));
+    cout<<random_number<<"\n \n";
+
+
+    // Generating Random Numbers in a Specific Range
+    int min = 1;
+    int max = 100;
+
+    for(int i = 1; i <= 10; i++) {
+        int random = min+(rand() % (max-min+1));
+        cout<<random<<endl;
+    }
+
     
 
     return 0;
