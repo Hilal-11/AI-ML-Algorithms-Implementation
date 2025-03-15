01b962e240abf37a2dcab7796be88bd40cdecabf
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-15 20:10:38 +0530

    generating random

--- cpp_random.cpp
+++ cpp_random.cpp
@@ -12,8 +12,23 @@ int main() {
     //     cout<<rand() % (1 , 100 + 1)<<endl;
     // }
 
+    // generate random numbers wothout seeding
+    cout<<"Generate random numbers without seeding "<<endl;
+    for(int i = 0; i < 10; i++) {
+        int random = rand();
+        cout<<"random number = "<<random<<endl;
+    }
 
-    
+
+    // seed the random numbers generated with current time
+    srand(time(0));
+    cout<<"\nGenerate random numbers with seeding "<<endl;
+    for(int i = 0; i < 10; i++){
+        int result = rand();
+        cout<<"random number with seeding = "<<result<<endl;
+    }
+
+
     
 
     return 0;
