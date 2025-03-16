69ca24cc6c4c5d1da338e29547ccbdf20b026bcf
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-17 02:31:43 +0530

    bst

--- Non Linear Data Structures/Trees/Binary Search Trees/BST.cpp
+++ Non Linear Data Structures/Trees/Binary Search Trees/BST.cpp
@@ -2,8 +2,34 @@
 using namespace std;
 
 
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+void inOrderTraversal(Node* root) {}
+
 int main() {
 
-    cout<<"Binary Search Trees"<<endl;
+    Node* root = new Node(50);   
+    root->left = new Node(40);
+    root->right = new Node(60);
+
+    root->left->left = new Node(30);
+    root->left->right = new Node(42);
+    root->left->left->left = new Node(10);
+    root->left->right->right = new Node(22);
+
+    root->right->left = new Node(55);
+    root->right->right = new Node(70);
+
     return 0;
 }
