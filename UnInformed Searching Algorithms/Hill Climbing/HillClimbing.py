a9337bef8d9ab5b29d947b812bea7e6337ff0d86
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-16 18:17:25 +0530

    hill climbing in .py

---
+++ UnInformed Searching Algorithms/Hill Climbing/HillClimbing.py
@@ -0,0 +1,30 @@
+import random
+
+def objectiveFunction(x):
+    return -((x - 3) * (x - 3) + 9);
+
+def hillClimbing(start_x , step_size = 0.1, max_iterations = 100):
+    current_x = start_x
+    current_value = objectiveFunction(current_x)
+    
+    
+    for i in range(0 , max_iterations):
+        new_x = current_x + (random.random() % 2 == 0)
+        if(new_x % 2 == 0):
+            current_x + step_size
+        else:
+            current_x + (-step_size)
+        new_value = objectiveFunction(new_x)
+            
+        if(new_value > current_value):
+            current_x = new_x
+            current_value = new_value
+        else:
+            break
+        
+    
+    return current_x
+            
+        
+
+    
