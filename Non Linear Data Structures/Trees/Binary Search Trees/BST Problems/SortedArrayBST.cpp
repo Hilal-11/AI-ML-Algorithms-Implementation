d49c9247d5876d18abb87650146ba25e44f8c1b3
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-17 22:34:23 +0530

    BST Problems

---
+++ Non Linear Data Structures/Trees/Binary Search Trees/BST Problems/SortedArrayBST.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+vector<int> sortedArrayBST(Node* root) {
+    if(root == NULL) return {};
+
+    vector<int> result;
+
+    sortedArrayBST(root->left);
+    result.push_back(root->data);
+    sortedArrayBST(root->right);
+
+    return result;
+
+}
+int main() {
+
+
+    Node* root = new Node(50);   
+    root->left = new Node(40);
+    root->right = new Node(60);
+
+    root->left->left = new Node(30);
+    root->left->right = new Node(42);
+    root->left->left->left = new Node(10);
+    root->left->left->right = new Node(32);
+
+    root->right->left = new Node(55);
+    root->right->right = new Node(70);
+
+    vector<int> result = sortedArrayBST(root);
+
+    for(int data : result) {
+        cout<<data<<"\t";
+    }
+
+    return 0;
+}
