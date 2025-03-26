19d6a6f69cf4c876b0e39d7a44526d7799f6ad65
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-26 18:16:56 +0530

    adjacency matrix

---
+++ Non Linear Data Structures/Graphs/Adjacency Matrics/C++/adjacency_matrics.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int n;
+    int m;
+    cin>> n >> m;
+    
+    int adjacencyMatrix[n+1][m+1];
+    
+    for(int i = 0; i < m; i++) {
+        int u , v;
+        cin>>u >> v;
+
+    }
+
+    return 0;
+}
