5098725d722d0f08925e660a03437377fca2d51c
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-26 22:29:01 +0530

    adjacenncy

--- Non Linear Data Structures/Graphs/Adjacency Matrics/C++/adjacency_matrics.cpp
+++ Non Linear Data Structures/Graphs/Adjacency Matrics/C++/adjacency_matrics.cpp
@@ -1,22 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int main () {
-    int n = 5;
-    int m = 6;
-    // cin>> n >> m;
-    
-    int adjacencyMatrix[5+1][6+1];
+int main() {
 
+    int n = 5;
+    int m = 6; 
+    
+    int adjacencyMatrics[5+1][6+1];
     for(int i = 0; i < m; i++) {
         int u , v;
         cin>>u >> v;
 
-        adjacencyMatrix[u][v] = 1;
-        adjacencyMatrix[v][u] = 1;
-        
-
+        adjacencyMatrics[u][v] = 1;
+        adjacencyMatrics[v][u] = 1; // For Undirected Graph
     }
+
+
 
     return 0;
 }
