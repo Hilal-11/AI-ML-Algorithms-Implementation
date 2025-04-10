a03b61953f8ff91c5ae06f81463923bc1880067b
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-04-10 20:21:06 +0530

    adjscency list of graph

---
+++ Non Linear Data Structures/Graphs/Adjacency List/C++/adjacencyList.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<list>
+#include<map>
+using namespace std;
+
+
+class Graph{
+    public: 
+        map<int , list<int>> adjscencyList;
+
+        void addEdge(int u , int v , bool direction) {
+            adjscencyList[u].push_back(v);
+            if(direction == 0)
+                adjscencyList[v].push_back(u);
+        }
+
+        void displayAdjacencyList() {
+            for(auto i : adjscencyList) {
+                cout<<i.first<<": ";
+                for(auto j: i.second) {
+                    cout<<j<<"\t";
+                }
+                cout<<endl;
+            }
+        }
+};
+
+int main() {
+    int vertices;
+    cout<<"Enter Vertices:- ";
+    int edges;
+    cout<<"Enter Edges:- ";
+    
+    Graph G;
+
+    for(int i = 0; i < edges; i++) {
+        int u , v;
+        cin>>u>>v;
+
+        G.addEdge(u , v , 0);
+    }
+
+    G.displayAdjacencyList();
+
+    
+    return 0;
+}
