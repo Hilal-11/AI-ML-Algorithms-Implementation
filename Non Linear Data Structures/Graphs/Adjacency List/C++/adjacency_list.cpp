4e04f0b21f7847224a0b96d0d7dfeaf62a2ab49f
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-04-10 19:28:27 +0530

    adj list repr graph

--- Non Linear Data Structures/Graphs/Adjacency List/C++/adjacency_list.cpp
+++ Non Linear Data Structures/Graphs/Adjacency List/C++/adjacency_list.cpp
@@ -1,10 +1,47 @@
 #include<iostream>
-
+#include<unordered_map>
+#include<list>
 using namespace std;
 
+class Graph{
+    public: 
+        unordered_map<int list<int>> adjacencyList;
+
+    void addEdge(int u , int v , bool direction) {
+        adjacencyList[u].push_back(v);
+
+        if(direction == 0)
+            adjacencyList[v].push_back(u);
+    }
+
+    void displayAjacencyList() {
+        for(auto i : adjacencyList){
+            cout<<i.first<<" ";
+            for(auto j: i.second){
+                cout<<j<<"\t";
+            }
+            cout<<endl;
+        }
+    }
+};
 
 int main() {
 
+    int vertices;
+    cout<<"Enter number of vertices:- "<<endl;
+    cin>>vertices;
+
+    int edges;
+    cout<<"Enter number of edges:- "<<endl;
+    cin>>edges;
+    Graph G;
+    for(int i = 0; i < edges; i++){
+        int u , v;
+        cin>>u>>v;
+        G.addEdge(u , v , 0);
+    }
+
+    G.displayAjacencyList();
     
     return 0;
 }
